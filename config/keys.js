b5bb3a7fd32ddcb8f6bd814618788820e04345b9
Chaithanya Bommavaram <chaithanyasaibommavaram@gmail.com>
2025-03-21 17:25:44 -0400

    Initial commit with express route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://chaithanyasaibommavaram:cmZ1ZbhFfwxL6IQs@personalprojects.v5w7a.mongodb.net/",
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/interview");
+
+const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello! Chay"));
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/interview", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
